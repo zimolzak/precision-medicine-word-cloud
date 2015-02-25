5fd00f3820a4bd359b7d525819b4b1b766bc4c85
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-02-25 09:20:06 -0500

    De-pluralize script finds more words by removing punctuation better.

--- end_s.pl
+++ end_s.pl
@@ -3,6 +3,13 @@
 use strict;
 while(<>){
     tr/A-Z/a-z/;
+    s/[:,.;"()*]//g;
+    s/&/and/g;
+    s/-/ /g;
+    s{/}{ }g;
+    s/“//g; #tricky char
+    s/”//g; #tricky char
+    s/—/ /g; #tricky char
     my @words = split(/ /);
     foreach my $x (@words) {
 	print "$x\n" if $x =~ /s$/;
