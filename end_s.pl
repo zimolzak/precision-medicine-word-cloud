c38ada9e135920150410bb72897c93fd9b8a0b21
Andrew Zimolzak <andyzimolzak@gmail.com>
2015-02-25 09:00:19 -0500

    Get to final curated list of inflected forms ending in S.

--- end_s.pl
+++ end_s.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl -w
+# usage: ./end_s.pl precision\ defs.txt | sort | uniq > s_words.txt
 use strict;
 while(<>){
+    tr/A-Z/a-z/;
     my @words = split(/ /);
     foreach my $x (@words) {
 	print "$x\n" if $x =~ /s$/;
